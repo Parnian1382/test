80bbb0238638877b96e14c9a624805ed5cd36d6a
parnian <milkovichue@gmail.com>
2023-02-22 12:29:02 +0330

    hello world

---
+++ workspace/HelloWorld/ir/ac/kntu/HelloWorld.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu;
+class HelloWorld{
+public static void main(String[]args){
+System.out.println("hello world");
+}
+}
+//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
